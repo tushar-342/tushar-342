e4841e13e3a1c11e2bd9bd3d4011ced025f11278
Tushar Kumar <tusharkrverma224@gmail.com>
2026-09-11 19:38:55 +0530

    Time: 4 ms (93.28%) | Memory: 44.2 MB (79.23%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,23 +1,23 @@
 class Solution {
     public String reverseWords(String s) {
-        int i = s.length() - 1;
+        int i = s.length()-1;
         StringBuilder sb = new StringBuilder();
         while(i >= 0){
-            while(i >=0 && s.charAt(i) == ' '){
-                i--;
-            }
-            if(i < 0) break;
-            int end = i;
-            while(i >=0 && s.charAt(i) != ' '){
-                i--;
-            }
-            if(sb.length() > 0){
-                sb.append(' ');
-            }
-            String word = s.substring(i+1, end+1);
-            sb.append(word);
+        while(i>=0 && s.charAt(i) == ' '){
+            i--;
         }
+        if(i < 0) break;
 
+        int end = i; 
+        while(i >=0 && s.charAt(i) != ' '){
+            i--;
+        }
+        if(sb.length() > 0){
+            sb.append(' ');
+        }
+        String word = s.substring(i+1, end+1);
+        sb.append(word);
+        }
         return sb.toString();
     }
 }
