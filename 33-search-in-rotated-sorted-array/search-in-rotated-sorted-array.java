caf25829e73bd7cc94a6df751680c9da1511aa8e
Tushar Kumar <tusharkrverma224@gmail.com>
2026-09-12 21:03:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (82.26%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int n = nums.length;
+        int low = 0, high = n - 1;
+
+        while (low <= high) {
+
+            int guess = (low + high) / 2;
+
+            if (nums[guess] == target) {
+                return guess;
+            }
+
+            if (nums[guess] > nums[n - 1]) {   // PART-1
+
+                if (nums[guess] > target) {
+                    if (nums[0] <= target) {
+                        high = guess - 1;
+                    } else {
+                        low = guess + 1;
+                    }
+                } else {
+                    low = guess + 1;
+                }
+
+            } else {                            // PART-2
+
+                if (nums[guess] < target) {
+                    if (nums[n - 1] >= target) {
+                        low = guess + 1;
+                    } else {
+                        high = guess - 1;
+                    }
+                } else {
+                    high = guess - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
