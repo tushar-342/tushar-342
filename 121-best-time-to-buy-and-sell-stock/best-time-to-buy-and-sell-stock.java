4d66e07ef7c4429c832d52268550d1ce4429e924
Tushar Kumar <tusharkrverma224@gmail.com>
2026-09-21 20:38:05 +0530

    Time: 18 ms (1.52%) | Memory: 92.9 MB (97.70%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -3,23 +3,18 @@ class Solution {
         int n = prices.length;
         int k = 2;
         int[][] dp = new int[n+1][k+1];
-        for(int i =0; i<n; i++){
-            Arrays.fill(dp[i], -1);
+        //Already intialize your dp graph with 0 in java
+        for(int i=n-1; i>=0; i--){
+            //k==2 -> buy
+            int c1 = dp[i+1][1]-prices[i]; //buy
+            int c2 = dp[i+1][2]; //skip
+            dp[i][2] = Math.max(c1,c2);
+
+            //k == 1-> sell
+            c1 = dp[i+1][0]+prices[i]; //sell
+            c2 = dp[i+1][1];
+            dp[i][1] = Math.max(c1,c2);
         }
-        return fun(prices, 0, n, k, dp);
-        
-    }
-    int fun(int[] prices, int i, int n, int k, int[][] dp){
-        if(i==n || k==0) return 0;
-        if(dp[i][k] != -1) return dp[i][k];
-        if(k==2){  //buy
-            int c1 = fun(prices, i+1, n, k-1, dp)-prices[i];
-            int c2 = fun(prices, i+1, n, k, dp);
-            return dp[i][k] = Math.max(c1, c2);
-        }else{  //sell
-            int c1 = fun(prices, i+1, n, k-1, dp)+prices[i];
-            int c2 = fun(prices, i+1, n, k, dp);
-            return dp[i][k] = Math.max(c1,c2);
-        }
+        return dp[0][2];
     }
 }
