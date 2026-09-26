b0ed5650b60b55f5d89e7f9332d8ab7a2a4ea6f2
Tushar Kumar <tusharkrverma224@gmail.com>
2026-09-26 16:16:09 +0530

    Time: 1 ms (99.94%) | Memory: 94.4 MB (51.59%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,20 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int k = 2;
-        int[][] dp = new int[n+1][k+1];
-        //Already intialize your dp graph with 0 in java
-        for(int i=n-1; i>=0; i--){
-            //k==2 -> buy
-            int c1 = dp[i+1][1]-prices[i]; //buy
-            int c2 = dp[i+1][2]; //skip
-            dp[i][2] = Math.max(c1,c2);
-
-            //k == 1-> sell
-            c1 = dp[i+1][0]+prices[i]; //sell
-            c2 = dp[i+1][1];
-            dp[i][1] = Math.max(c1,c2);
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for(int i=0; i<prices.length; i++){
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
         }
-        return dp[0][2];
+        return maxProfit;
     }
 }
